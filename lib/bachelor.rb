d112c652d765c01b57321956513d7f750eabc069
Melissa Nock <melnock@gmail.com>
2018-01-22 20:07:33 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,69 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+ winner = []
+data.each {|seasons, array|
+  if seasons == season
+  array.each {|hash|
+      if hash["status"] == "Winner"
+        winner << hash.values_at("name")
+      end 
+    }
+  end
+  }
+ name = winner[0][0].split
+ name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  work = ""
+data.each {|seasons, array|
+  array.each{|hash|
+        if hash["occupation"] == occupation
+          work = hash.values_at("name")
+        end 
+    }
+}
+work[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  home = []
+data.each {|seasons, array|
+  array.each{|hash|
+        if hash["hometown"] == hometown
+          home << hash.values_at("name")
+        end 
+    }
+}
+home.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  home = []
+data.each {|seasons, array|
+  array.each{|hash|
+        if hash["hometown"] == hometown
+          home << hash.values_at("occupation")
+        end 
+    }
+}
+home[0][0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+   age = []
+data.each {|seasons, array|
+  if seasons == season
+  array.each {|hash|
+     
+        age << hash.values_at("age")
+     
+    }
+  end
+  }
+  length = age.length
+  total = 0
+  age.each{|x| int= x[0].to_f
+    total = total + int
+  }
+  average = (total/length).round
 end

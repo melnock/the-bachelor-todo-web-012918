9d106cfd7c863137b21898555a6409ef82ba2e87
Melissa Nock <melnock@gmail.com>
2018-01-22 18:55:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
 data[season].each {|array|
   array.each{|hash, tidbits|
       if tidbits["status"] == "Winner"
-        winner = tidbits["name"].split
+        winner = tidbits.values_at("name").split
       end 
     }
     
